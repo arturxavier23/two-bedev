d3923d8364feb4dfe1c50d409ff7eab6c95813ab
Nicolly Mendes <nicollycesco22@gmail.com>
2026-04-29 16:56:16 -0400

    fix: remove toaster e sonner nao utilizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Opening from "./pages/Opening";
 import Login from "./pages/Login";
@@ -18,8 +16,6 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Opening />} />
